5c72e29f9eeb460c299812cd3092e52422751c67
Nikita Izygin <nr.izygin@tensor.ru>
2015-08-14 16:16:51 +0300

    tmpl min addded

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,26 @@ module.exports = [
           library: ["tmpl"],
           libraryTarget: "umd"
       }
+  },
+  {
+    target: 'web',
+      entry: {
+          tmpl: "./js/entry"
+      },
+      plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        })
+      ],
+      output: {
+          // Make sure to use [name] or [id] in output.filename
+          //  when using multiple entry points
+          filename: "[name].min.js",
+          chunkFilename: "[id].min.js",
+          library: ["tmpl"],
+          libraryTarget: "umd"
+      }
   }
 ]
