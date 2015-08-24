78aae5d95673ece65e40bd19a15d3615e6e3af16
Nikita Izygin <nr.izygin@tensor.ru>
2015-08-24 13:10:31 +0300

    testing

--- test/html_test.js
+++ test/html_test.js
@@ -3,14 +3,13 @@ var tmpl = require('../tmpl'),
     expect = require('chai').expect;
 describe('Objects and children', function () {
   it('Testing children vars', function (done) {
-    var parsed = tmpl.parse('<div>123 {{ master }}</div>'),
-        data = { master: 321 };
-    tmpl.traverse(parsed, data).handle(function (traversed) {
+    var parsed = tmpl.parse('<div>123 {{ master }}</div>');
+    tmpl.traverse(parsed).handle(function (traversed) {
       setTimeout(function() {
         var child = traversed[0].children[0];
         for (var i = 0; i < child.data.length; i++) {
-          if (child.data[i].type === 'var') {
-            assert.equal(child.data[i].value, data.master);
+          if (child.data[i].name === 'master') {
+            assert.equal(child.data[i].type, 'var');
           }
         }
         done();
@@ -20,15 +19,9 @@ describe('Objects and children', function () {
   it('Testing For module', function (done) {
     var parsed = tmpl.parse('<for data="dog in dogs"><span class="{{ dog.type }}">{{dog.name}}</span></for>');
         data = {dogs: [{type: 'big', name: 'Lacy'}, {type: 'small', name: 'Kev'}, {type: 'stupid', name: 'Mike'}]};
-    tmpl.traverse(parsed, data).handle(function (traversed) {
+    tmpl.traverse(parsed).handle(function (traversed) {
         setTimeout(function () {
-          var classData = traversed[0].attribs.class.data;
-          expect(traversed).to.have.length(3);
-          for (var i = 0; i < classData.length; i++) {
-            if (classData[i].type === 'var') {
-              expect(classData[i].value).to.equal('big');
-            }
-          }
+          expect(tmpl.html(traversed, data)).to.equal('<span class="big">Lacy</span><span class="small">Kev</span><span class="stupid">Mike</span>');
           done();
         });
     });
@@ -36,9 +29,9 @@ describe('Objects and children', function () {
   it('Testing If module', function (done) {
     var parsed = tmpl.parse('<if data="number === 124"><div class="{{bam}}">{{tt}}</div></if>');
         data = {number: 124, bam: 'hidden'};
-    tmpl.traverse(parsed, data).handle(function (traversed) {
+    tmpl.traverse(parsed).handle(function (traversed) {
         setTimeout(function () {
-          expect(traversed).to.have.length(1);
+          expect(tmpl.html(traversed, data)).to.equal('<div class="hidden"></div>')
           done();
         });
     });
