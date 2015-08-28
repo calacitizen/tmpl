0ba0c1d35014979ad77479f785442b54281150a3
Nikita Izygin <nr.izygin@tensor.ru>
2015-08-28 18:35:44 +0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,35 @@
 var webpack = require("webpack");
-module.exports = [
-  {
-    target: 'web',
-      entry: {
-          tmpl: "./js/entry"
-      },
-      output: {
-          // Make sure to use [name] or [id] in output.filename
-          //  when using multiple entry points
-          filename: "[name].js",
-          chunkFilename: "[id].js",
-          library: ["tmpl"],
-          libraryTarget: "umd"
-      }
+module.exports = [{
+  target: 'web',
+  entry: {
+    tmpl: "./js/entry"
   },
-  {
-    target: 'web',
-      entry: {
-          tmpl: "./js/entry"
-      },
-      plugins: [
-        new webpack.optimize.UglifyJsPlugin({
-            compress: {
-                warnings: false
-            }
-        })
-      ],
-      output: {
-          // Make sure to use [name] or [id] in output.filename
-          //  when using multiple entry points
-          filename: "[name].min.js",
-          chunkFilename: "[id].min.js",
-          library: ["tmpl"],
-          libraryTarget: "umd"
-      }
+  output: {
+    // Make sure to use [name] or [id] in output.filename
+    //  when using multiple entry points
+    filename: "[name].js",
+    chunkFilename: "[id].js",
+    library: ["tmpl"],
+    libraryTarget: "umd"
   }
-]
+}, {
+  target: 'web',
+  entry: {
+    tmpl: "./js/entry"
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ],
+  output: {
+    // Make sure to use [name] or [id] in output.filename
+    //  when using multiple entry points
+    filename: "[name].min.js",
+    chunkFilename: "[id].min.js",
+    library: ["tmpl"],
+    libraryTarget: "umd"
+  }
+}]
